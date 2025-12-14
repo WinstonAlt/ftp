ceb8bc70ee937783b3184c38e0624fd3c85f6215
WinstonAlt <caowinston0@outlook.com>
2025-12-14 13:51:07 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-echo "Preparing..."
-mv /usr/bin/flatpak /usr/bin/flatpak-real
-echo "Installing..."
-curl -o /usr/bin/flatpak https://github.com/WinstonAlt/ftp/raw/refs/heads/main/flatpak
+sudo echo "Preparing..."
+sudo mv /usr/bin/flatpak /usr/bin/flatpak-real
+sudo echo "Installing..."
+sudo curl -o /usr/bin/flatpak https://github.com/WinstonAlt/ftp/raw/refs/heads/main/flatpak
 echo "Done installing"
 echo
 echo "To use type in 'flatpak'"
 echo
 echo "--------------------------------------------------------"
 echo "--------------------Made by Winston---------------------"
-echo "---------Github: https://github.com/WinstonAlt"---------"
+echo "---------Github: https://github.com/WinstonAlt---------"
 echo "--------------------------------------------------------"
